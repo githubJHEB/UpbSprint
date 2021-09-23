671d8a722ea295782f3dfad3f58609da94b8d650
Jorge Villalba <88755360+Jorge-Villalba@users.noreply.github.com>
2021-09-22 21:31:36 -0500

    Create validaciones.js
    Creacion Validaciones

---
+++ public_html/js/validaciones.js
@@ -0,0 +1,10 @@
+function validar_nombre_usuario(string){
+}
+function validar_contrasena(string){
+}
+function confirmar_contrasena(string){
+}
+
+module.exports.validar_nombre_usuario=validar_nombre_usuario;
+module.exports.validar_contrasena=validar_contrasena;
+module.exports.confirmar_contrasena=confirmar_contrasena;
