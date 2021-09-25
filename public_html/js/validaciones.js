08d4932157d14767ee505eba961557cc64427e0d
jaimeandres27 <robertoarroyavetecnologia@gmail.com>
2021-09-24 21:33:06 -0500

    agrego validar_contraseña
    Funciona la parte de restringir a solo letras y numerps. aún no realiza la validación de longitud. JE

--- public_html/js/validaciones.js
+++ public_html/js/validaciones.js
@@ -1,6 +1,27 @@
 function validar_nombre_usuario(string){
 }
 function validar_contrasena(string){
+     var key = string.keyCode,
+  tecla = String.fromCharCode(key).toLowerCase(),
+  letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789",
+  especiales = [8, 37, 39, 46],
+  tecla_especial = false;
+  
+for (var i in especiales) {
+  if (key == especiales[i]) {
+    tecla_especial = true;
+    break;
+  }
+}
+
+if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+  return false;
+}
+if (nombre.length<6){
+  return false;
+}else{
+  return true;
+}    
 }
 function confirmar_contrasena(string){
 }
